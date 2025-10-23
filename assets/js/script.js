6dd881045bba65de0973996b320a9b6f72dcc537
King Ridwan <akhmadridwan357@gmail.com>
2025-10-23 22:51:07 +0700

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.style.transform = 'scale(1.05)';
-    });
-    card.addEventListener('mouseleave', () => {
-        card.style.transform = 'scale(1)';
-    });
-});
